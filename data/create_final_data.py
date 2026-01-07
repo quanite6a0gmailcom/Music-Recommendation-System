17b9082043fec678d65f4c6fd70e3f6ab5921444
Le Van Quan <quanle12a0@gmail.com>
2026-01-07 10:43:48 +0700

    crawling lyrics and cleanning data new

--- data/create_final_data.py
+++ data/create_final_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 SOURCE_FILE = r'spotify_songs.csv/spotify_songs_processed.csv'
 FINAL_FILE = r'spotify_songs.csv/spotify_songs_final.csv'
 
-cols_to_use = ['track_artist','track_name','track_popularity','playlist_genre','energy','valence','acousticness','instrumentalness','speechiness']
+cols_to_use = ['track_artist','track_name','track_popularity','playlist_genre','energy','valence','acousticness','instrumentalness','speechiness','lyrics']
 
 df = pd.read_csv(SOURCE_FILE,usecols=cols_to_use,encoding='utf-8-sig')
 
